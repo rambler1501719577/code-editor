a08c84d730a0c0f62473727df5863cc593cd025c
苏浩 <1501719577@qq.com>
2024-10-15 14:42:17 +0800

    1

--- src/lib/core/extra-function.js
+++ src/lib/core/extra-function.js
@@ -15,7 +15,7 @@
 /**
  * textarea 插入内容
  */
-export const insertTextAtCaret = (obj, {prefix, subfix, str, type}, $vm) => {
+export const insertTextAtCaret = (obj, { prefix, subfix, str, type }, $vm) => {
     obj.focus()
     if (isExistSelectionObj(obj)) {
         var startPos = obj.selectionStart;
@@ -59,7 +59,7 @@ export const insertCodeBlock = ($vm) => {
     let obj = $vm.getTextareaDom();
 
     if (isExistSelectionObj(obj)) {
-        const {defaultLanguageText: language} = code_structure;
+        const { defaultLanguageText: language } = code_structure;
         let { selectionStart: startPos, selectionEnd: endPos, value: tmpStr } = obj;
 
         if (startPos === endPos) {
@@ -87,7 +87,7 @@ export const insertCodeBlock = ($vm) => {
 }
 
 function insertCodeBlockToVM(language, content, selectionObj) {
-    const {prefix, subfix} = code_structure;
+    const { prefix, subfix } = code_structure;
     let { selectionStart: startPos, selectionEnd: endPos, value: tmpStr } = selectionObj;
 
     let value = tmpStr.substring(0, startPos);
@@ -98,20 +98,19 @@ function insertCodeBlockToVM(language, content, selectionObj) {
 
     selectionObj.value = value;
     selectionObj.selectionStart = startPos + prefix.length + (language ? 0 : 1);
-    selectionObj.selectionEnd = selectionObj.selectionStart + language.length +  content.length;
+    selectionObj.selectionEnd = selectionObj.selectionStart + language.length + content.length;
 }
 
 function removeCodeBlockFromVM(selectionObj) {
-    let {prefix, subfix, defaultLanguageText: language} = code_structure;
+    let { prefix, subfix, defaultLanguageText: language } = code_structure;
     let { selectionStart: startPos, selectionEnd: endPos, value: content } = selectionObj;
     let selectedValue = content.substring(startPos, endPos);
 
-    if (content.substring(startPos - 1, startPos) === "\n")
-    {
+    if (content.substring(startPos - 1, startPos) === "\n") {
         prefix = prefix + "\n";
     } else {
         subfix = "\n" + subfix;
-        if (prefix + language + subfix === content.substring(startPos - prefix.length,endPos + subfix.length)) {
+        if (prefix + language + subfix === content.substring(startPos - prefix.length, endPos + subfix.length)) {
             let value = content.substring(0, startPos - prefix.length);
             value += content.substring(endPos + subfix.length, content.length)
 
@@ -137,22 +136,21 @@ function isExistSelectionObj(textareaDom) {
 
 function isCancelCodeBlock(selectionObj) {
     let { selectionStart: startPos, selectionEnd: endPos, value: content } = selectionObj;
-    let {prefix, subfix} = code_structure;
+    let { prefix, subfix } = code_structure;
 
-    if (content.substring(startPos - 1, startPos) === "\n")
-    {
+    if (content.substring(startPos - 1, startPos) === "\n") {
         prefix = prefix + "\n";
     } else {
         subfix = "\n" + subfix;
     }
 
     return content.substring(startPos - prefix.length, startPos) === prefix &&
-                content.substring(endPos, endPos + subfix.length) === subfix;
+        content.substring(endPos, endPos + subfix.length) === subfix;
 }
 
 // 处理粗体与斜体冲突问题
 function judgeItalicAndBold(prefix, subfix, tmpStr, startPos, endPos) {
-    if (prefix === '*' && subfix ===  '*') {
+    if (prefix === '*' && subfix === '*') {
         if (tmpStr.substring(startPos - 2, startPos - 1) === '*' && tmpStr.substring(endPos + 1, endPos + 2) === '*') {
             return false
         }
@@ -213,7 +211,7 @@ export const removeLine = ($vm) => {
         if (end < tmpStr.length) {
             end++
         }
-        obj.value = tmpStr.substring(0, start)  + tmpStr.substring(end, tmpStr.length);
+        obj.value = tmpStr.substring(0, start) + tmpStr.substring(end, tmpStr.length);
         obj.selectionEnd = obj.selectionStart = start === 0 ? 0 : start - 1;
     } else {
         alert('Error: Browser version is too low')
@@ -295,7 +293,7 @@ export const unInsertTab = ($vm, tab) => {
         let lastLine = tmpStr.substring(0, startPos).split('\n').pop()
         if (lastLine.search(regTab) >= 0) {
             // 替换最后一个制表符为空
-            obj.value = tmpStr.substring(0, startPos - lastLine.length) +  lastLine.replace(regTab, '') + tmpStr.substring(endPos, tmpStr.length);
+            obj.value = tmpStr.substring(0, startPos - lastLine.length) + lastLine.replace(regTab, '') + tmpStr.substring(endPos, tmpStr.length);
             obj.selectionStart = obj.selectionEnd = startPos - (tab || 1);
         }
     } else {
@@ -351,7 +349,7 @@ export const insertEnter = ($vm, event) => {
 /**
  * 生成导航目录
  */
-export const getNavigation = ($vm , full) => {
+export const getNavigation = ($vm, full) => {
     let navigationContent;
 
     navigationContent = $vm.$refs.navigationContent
@@ -360,10 +358,10 @@ export const getNavigation = ($vm , full) => {
     let nodes = navigationContent.children
     if (nodes.length) {
         for (let i = 0; i < nodes.length; i++) {
-            judageH(nodes[i] , i , nodes)
+            judageH(nodes[i], i, nodes)
         }
     }
-    function judageH(node , i , nodes) {
+    function judageH(node, i, nodes) {
         let reg = /^H[1-6]{1}$/;
         if (!reg.exec(node.tagName)) {
             node.style.display = 'none'
@@ -452,11 +450,11 @@ export const windowResize = ($vm) => {
 
 export function loadScript(src, callback) {
     if (!(typeof callback === 'function')) {
-        callback = function() {};
+        callback = function () { };
     }
     var check = document.querySelectorAll("script[src='" + src + "']");
     if (check.length > 0) {
-        check[0].addEventListener('load', function() {
+        check[0].addEventListener('load', function () {
             callback();
         });
         callback();
@@ -485,7 +483,7 @@ export function loadScript(src, callback) {
 // <link href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/agate.min.css" rel="stylesheet">
 export function loadLink(src, callback, id) {
     if (!(typeof callback === 'function')) {
-        callback = function() {};
+        callback = function () { };
     }
     var check = document.querySelectorAll("link[href='" + src + "']");
     if (check.length > 0) {
